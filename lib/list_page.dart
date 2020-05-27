c661ac0bec9fbdf9f0ae40a863588cb48f371851
ChoiAhHyun <cah1113@naver.com>
2020-05-27 22:21:28 +0900

    Get memo list from firestore

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'edit_page.dart';
 
@@ -13,17 +14,70 @@ class _ListPageState extends State<ListPage> {
       appBar: AppBar(
         title: Text('MemoApp'),
         actions: <Widget>[
-          new IconButton(icon: Image.asset('assets/baseline_add_white_24.png'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EditPage()),
-                );
-              },
+          new IconButton(
+            icon: Image.asset('assets/baseline_add_white_24.png'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => EditPage()),
+              );
+            },
           ),
         ],
       ),
       backgroundColor: Colors.grey,
+      body: StreamBuilder(
+        stream: Firestore.instance.collection("memo").orderBy("datetime", descending: true).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: const Text("Loading..."));
+          }
+          return GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                margin: EdgeInsets.all(1),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 14, right: 14, top: 14),
+                      child: Text(
+                        snapshot.data.documents[index]["title"],
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                        softWrap: false,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                          left: 14, right: 14, top: 14, bottom: 14),
+                      child: Text(
+                        snapshot.data.documents[index]["content"],
+                        maxLines: 5,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                        ),
+                        softWrap: false,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            itemCount: snapshot.data.documents.length,
+          );
+        },
+      ),
     );
   }
 }
